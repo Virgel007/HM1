c32d7e86af65a1151e380ccc174d67c3f77e97d3
Virgel007 <neokrium.88@gmail.com>
2023-10-16 16:41:53 +0300

    Комит с задача 1-4

--- src/Main.java
+++ src/Main.java
@@ -67,6 +67,24 @@ public class Main {
         var sportDrink = (bananWeight + milkWeight + iceCreamWeight + rawEggsWeight) / 1000f;
 
         System.out.println("Вес спорт-завтрака " + sportDrink + " кг.");
+        //### **Задача 4.**
+        //
+        //Правила соревнований обновились, и теперь нашему спортсмену нужно сбросить 7 кг, чтобы оставаться в своей весовой категории.
+        //
+        //Тренер скорректировал рацион так, чтобы спортсмен мог терять в весе от 250 до 500 грамм в день.
+        //
+        //Посчитайте, сколько дней уйдёт на похудение, если спортсмен будет терять каждый день по 250 грамм и сколько, если каждый день будет худеть на 500 грамм.
+        //
+        //Посчитайте, сколько может потребоваться дней в среднем, чтобы добиться результата похудения.
+        //
+        //Результаты подсчетов выведите в консоль.
+        var sportManWeightLoss = 7;
+        var sportManTypeA = 250;
+        var sportManTypeB = 500;
+        var WeightLossTypeA = (sportManWeightLoss * 1000) / sportManTypeA;
+        var WeightLossTypeB = (sportManWeightLoss * 1000) / sportManTypeB;
+        System.out.println("Дней уйдет чтобы сбросить " + sportManWeightLoss +" кг. " + WeightLossTypeA + " дней." + " теряя в весе "  +sportManTypeA + " гр.");
+        System.out.println("Дней уйдет чтобы сбросить " + sportManWeightLoss +" кг. " + WeightLossTypeB + " дней." + " теряя в весе "  +sportManTypeB + " гр.");
 
 
     }
